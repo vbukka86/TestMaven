6d1a27ba3b7dcbd1987af098c7c769e3c55d8134
Vijay Chander <vbukka@Vijays-MacBook-Pro.local>
2022-02-15 22:59:57 -0600

    021522 - launchbrowser

--- src/test/java/com/testNg/testNGAnnotations.java
+++ src/test/java/com/testNg/testNGAnnotations.java
@@ -1,9 +1,15 @@
 package com.testNg;
 
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
@@ -11,24 +17,28 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
 public class testNGAnnotations {
+	WebDriver driver;
+	
 	@Test
-	public void test1() {
-		System.out.println("test1 Method");
+	public void launchBrowser() {
+		System.out.println("inside launch browser method");
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://demo.borland.com/gmopost/");
+		driver.manage().window().maximize();
+		
+		String actualTitle = driver.getTitle();
+		System.out.println("Title of the window:" + actualTitle);
+		Assert.assertTrue(actualTitle.equals("Welcome to Green Mountain Outpost"));
+		driver.close();
+		driver.quit();
 	}
 
 	@Test
-	public void test2() {
-		System.out.println("test2 Method");
-	}
-
-	@Test
-	public void test3() {
-		System.out.println("test3 Method");
-	}
-
-	@Test
-	public void Test4() {
-		System.out.println("test4 Method");
+	public void closeBrowser() {
+		System.out.println("inside close browser method");
+		
 	}
 
 	@BeforeMethod
